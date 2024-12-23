4e324a0a1188dd920215fa6eaaf2b57652df5e69
rameshghule11 <rameshghule112002@gmail.com>
2024-12-23 08:00:33 -0800

    js added

--- script.js
+++ script.js
@@ -2,7 +2,3 @@ function openModel(){
     const overlayEle = document.getElementById("overlay");
     overlayEle.style.display = "flex";
 }
-function closeModel(){
-    const overlayEle = document.getElementById("overlay");
-    overlayEle.style.display = "none";
-}
